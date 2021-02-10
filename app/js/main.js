1151319cb2485c79e1484f5c486c52eab32fdb97
aaksi <yyff91@mail.ru>
2021-02-10 14:39:57 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function(){
-  
+
+    // $('.slider').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: true,
+    //     fade: true,
+    //     asNavFor: '.menu-slider'
+    //   });
+    //   $('.menu-slider').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.slider',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    //   });
 
 });
